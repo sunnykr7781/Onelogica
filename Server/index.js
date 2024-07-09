10125cc7979010cea23f67d2abdefda2461f9963
Sunny Kumar <140904663+sunnykr7781@users.noreply.github.com>
2024-07-09 23:42:19 +0530

    Add files via upload

--- Server/index.js
+++ Server/index.js
@@ -1,30 +1,44 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.js'
-import profileRoute from './routes/profile.js';
-import isAvailableRouter from './routes/isAvailable.js';
-import postRouter from './routes/post.js';
-import commentRouter from './routes/comment.js';
-dotenv.config();
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import userRouter from "./routes/user.js"
+import profileRoute from "./routes/profile.js"
+import isAvailableRouter from "./routes/isAvailable.js"
+import postRouter from "./routes/post.js"
+import commentRouter from "./routes/comment.js"
+import connectDB from "./config/dbconfig.js"
 
-const Port = process.env.PORT
+dotenv.config()
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const port = process.env.PORT || 3000 // Use a default port if not specified
 
-app.use('/user', userRouter)
-app.use('/profile', profileRoute)
-app.use('/isAvailable', isAvailableRouter)
-app.use('/post', postRouter)
-app.use('/comment',commentRouter)
+const app = express()
+connectDB() // Connect to MongoDB
 
-mongoose.connect('mongodb://localhost:27017/socialMedia')
+const PORT = 4000
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-app.listen(Port, () => {
-    console.log("successfuly listening on Port number " + Port)
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server is running")
+})
+
+app.use("/user", userRouter)
+app.use("/profile", profileRoute)
+app.use("/isAvailable", isAvailableRouter)
+app.use("/post", postRouter)
+app.use("/comment", commentRouter)
+
+// MongoDB connection handling
+const db = mongoose.connection
+mongoose.connection.once("open", () => {
+  console.log("DB connected")
+  app.listen(3030, () => {
+    console.log("server is running")
+  })
 })
